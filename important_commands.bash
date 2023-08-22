49012c6f14edfb59b6eea2eba9e5a4a57bb89764
Muhammad Bilal Azam <124085891+mbilalazam@users.noreply.github.com>
2023-08-22 16:24:26 -0500

    Update important_commands.bash

--- important_commands.bash
+++ important_commands.bash
@@ -8,26 +8,7 @@ lar -c runsimdumpnew_dunend2x2.fcl out_gen_g4.root -n 1000
 .L plotTrackMult2x2wTruthMuCC.C
 plotTrackMult2x2wTruthMuCC()
 
-.L plotTrackMult2x2wTruthMuCuts.C
-plotTrackMult2x2wTruthMuCuts()
-
-.L plotTrackMult2x2wTruthmult.C
-plotTrackMult2x2wTruthmult()
-
-.L plotTrackMult2x2wTruthMuNC.C
-plotTrackMult2x2wTruthMuNC()
-
-.L plotTrackMult2x2wTruthMuNoCut.C
-plotTrackMult2x2wTruthMuNoCut()
-
 # Some commands to send email from terminal
 echo "These multiplicity plots are without any cut. Only cut is in mult_mu_c0p95.png for cos(theta)<0.95." | mutt -s "nocutonRange" -a *.png *.h *.C -- mazam3@hawk.iit.edu
-
-echo "These multiplicity plots are with cut on range (acceptance). An additional cut is in mult_mu_c0p95.png for cos(theta)<0.95." | mutt -s "withcutonRange" -a *.png *.h *.C -- mazam3@hawk.iit.edu
-
-echo "These are multiplicity plots with cut on particle's track length." | mutt -s "withcutonLength" -a *.png *.h *.C -- mazam3@hawk.iit.edu
-
-echo "These are  multiplicity plots with cuts on range (acceptance) and particle's track length." | mutt -s "withcutonRangeAndLength" -a *.png *.h *.C -- mazam3@hawk.iit.edu
-
 
 
