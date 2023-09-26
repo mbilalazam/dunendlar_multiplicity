fe76fbfe3b7e5ca20844f009121d4175e5362034
Muhammad Bilal Azam <124085891+mbilalazam@users.noreply.github.com>
2023-09-26 13:52:34 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create a new directory and setup environment variables
+cd /dune/app/users/mazam/production/MiniRun4_RHC
+
+# Set the language and locale variables to UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+
+# Source the DUNE software setup script
+source /grid/fermiapp/products/dune/setup_dune.sh
